77968cb88fd8c56b3d0ee51ea6fa2c881cefd57f
Naomi Hancock <naomi_hancock@yahoo.co.uk>
2024-07-22 19:05:21 +0000

    added tables

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -25,3 +25,9 @@ class Task(db.Model):
     due_date = db.Column(db.Date, nullable=False) 
     category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
 
+    def __repr__(self):
+        #__repr__ to represent itself in the form of a string
+        return "#{0} - Task: {1} | Urgent: {2}".format(
+            self.id, self.task_name, self.is_urgent
+        )
+        
